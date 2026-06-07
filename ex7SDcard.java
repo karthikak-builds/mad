061bacf6e3676efa23dba46292210c1d4a6959c4
Karthika K <karthikak.careerhub@gmail.com>
2026-06-07 12:02:37 +0530

    Change file saving method to MediaStore
    Refactor file saving to use MediaStore for external storage.

---
+++ ex7SDcard.java
@@ -0,0 +1,46 @@
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.editText);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(v -> {
+            try {
+                ContentValues cv = new ContentValues();
+                cv.put(MediaStore.MediaColumns.DISPLAY_NAME, "data.txt");
+                cv.put(MediaStore.MediaColumns.RELATIVE_PATH,
+                        Environment.DIRECTORY_DOWNLOADS);
+
+                Uri uri = getContentResolver().insert(
+                        MediaStore.Files.getContentUri("external"), cv);
+
+                OutputStream os = getContentResolver().openOutputStream(uri);
+                os.write(et.getText().toString().getBytes());
+                os.close();
+
+                Toast.makeText(this,
+                        "Saved in Downloads",
+                        Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
